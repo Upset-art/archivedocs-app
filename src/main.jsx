3e09b0419119cae5e2d5fab07e32ce5d704eb682
Upset-art <domestudios.contact@gmail.com>
2025-09-08 12:40:36 +0000

    #Enter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // ← Cette ligne est cruciale !
+import './index.css'   // ⬅️ important pour Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
