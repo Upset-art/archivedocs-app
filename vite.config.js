dc000474639c7086300496a2e99823ff943130fc
Upset-art <domestudios.contact@gmail.com>
2025-09-09 08:39:44 +0000

    #Enter

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const codespace = process.env.CODESPACE_NAME
-  ? `${process.env.CODESPACE_NAME}-5173.app.github.dev`
-  : 'localhost'
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     port: 5173,
-    strictPort: true,
+    host: '0.0.0.0',
     hmr: {
-      protocol: 'wss',
-      host: codespace,
-      clientPort: 443,
+      port: 5173,
     },
   },
+  base: './', // <-- ajoute cette ligne
 })
